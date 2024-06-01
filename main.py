63679c45de97a15624a4103c36fd5704ca11cfd5
VladislavZ <vlad.zorin003@gmail.com>
2024-06-01 12:07:49 +0200

    little rework

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import subprocess
 import os
 
+
 def run_pytest():
     folder_path = 'tests'
     tests = [f for f in os.listdir(folder_path) if f.endswith('.py')]
 
     for test in tests:
-        try:
-            result = subprocess.run(["pytest", f"{folder_path}/{test}"],
-                                    check=False,
-                                    text=True,
-                                    stdout=subprocess.PIPE
-                                    )
-        finally:
-            print('stdout:', result.stdout)
-        
+        result = subprocess.run(["pytest", f"{folder_path}/{test}"],
+                                check=False,
+                                text=True,
+                                stdout=subprocess.PIPE
+                                )
+
+        print('stdout:', result.stdout)
+
 if __name__ == '__main__':
     run_pytest()
